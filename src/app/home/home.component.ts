c1468058c01a2a4ead1514b20b382305c8c0cf94
cuoigl <cuoi.dev@gmail.com>
2022-10-04 13:47:40 +0700

    ComboBox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,20 +8,46 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = 'Cuoi';
   public age = 15;
+  
   public fruits = ['Apple', 'Orange'];
+
   public fruitItems = [
     { name: 'Apple', price: 12, sale: true },
     { name: 'Orange', price: 15, sale: false },
   ];
 
+  public citis = [
+    { city: 'Choose a city', districts: [] },
+    { city: 'Gia Lai', districts: ['Pleiku', 'ChưProng'] },
+    { city: 'Đà Nẵng', districts: ['Cẩm Lệ', 'Ngũ Hành Sơn'] },
+  ];
+
+  public districts : string[] = [];
+
   constructor() {}
 
   ngOnInit(): void {
-    console.log('Fruit = ', this.fruits);
   }
 
   public resetName(): void {
     this.name = '';
     console.log('resetName');
   }
+
+  public changeCity(event: any): void {
+    const city = event.target.value;  
+    if (!city) {
+      return;
+    }
+
+    // const search = this.citis.filter((data) => data.city === city);
+    // if ( search && search.length > 0) { 
+    //   this.districts = search[0].districts;
+    // }
+
+    this.districts = this.citis.find((data) => data.city === city)?.districts || [];
+  }
+
+
+
 }
