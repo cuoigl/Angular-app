28ad867a86c2480e01905f8e6194301026d3da21
cuoigl <cuoi.dev@gmail.com>
2022-10-04 10:41:59 +0700

    Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { OnsalePipe } from './onsale.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    OnsalePipe
   ],
   imports: [
     BrowserModule,
