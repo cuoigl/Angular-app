63fa60b453059590ee3ea75ef45263f0dcea3706
cuoigl <cuoi.dev@gmail.com>
2022-10-04 14:27:35 +0700

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { OnsalePipe } from './onsale.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    OnsalePipe
+    OnsalePipe,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
